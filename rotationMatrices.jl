ba0cfa56105dbb751d2d6774eba62c875a9c51ea
kjl <keithjl@mit.edu>
2023-02-07 18:13:59 -0500

    add files

---
+++ rotationMatrices.jl
@@ -0,0 +1,165 @@
+"""
+Rotation matrix for a 2D truss element
+
+Cx, Cy are the directional cosines of the element in global coordinates
+
+Cx, Cy = (endPosition - startPosition) / length
+
+IE they are the components of the normalized vector that represents the element.
+"""
+function R2DTruss(Cx::Real, Cy::Real)
+    return [Cx Cy 0 0 ; 0 0 Cx Cy]
+end
+
+"""
+Rotation matrix for a 2D truss using start and end points
+
+positionStart = [xstart, ystart]: position of starting node
+positionEnd = [xend, yend]: position of ending node
+L: length of element
+"""
+function R2DTruss(positionStart::Vector{Real}, positionEnd::Vector{Real}, L::Real)
+
+    Cx, Cy = (positionEnd .- positionStart) ./ L
+
+    return [Cx Cy 0 0 ; 0 0 Cx Cy]
+end
+
+"""
+Rotation matrix for a 3D truss element
+Cx, Cy, Cz are the directional cosines of the element in global coordinates
+Cx, Cy, Cz = (endPosition - startPosition) / length
+"""
+function R3DTruss(Cx::Real, Cy::Real, Cz::Real)
+
+    return [Cx Cy Cz 0 0 0; 0 0 0 Cx Cy Cz]
+
+end
+
+"""
+Rotation matrix for a 3D truss using start and end points
+
+positionStart = [xstart, ystart, zstart]: position of starting node
+positionEnd = [xend, yend, zend]: position of ending node
+L: length of element
+"""
+function R2DTruss(positionStart::Vector{Real}, positionEnd::Vector{Real}, L::Real)
+
+    Cx, Cy, Cz = (positionEnd .- positionStart) ./ L
+
+    return [Cx Cy Cz 0 0 0; 0 0 0 Cx Cy Cz]
+end
+
+"""
+Rotation matrix for a 2D frame element
+
+Cx, Cy are the directional cosines of the element in global coordinates
+
+Cx, Cy = (endPosition - startPosition) / length
+
+IE they are the components of the normalized vector that represents the element.
+"""
+function R2DFrame(Cx::Real, Cy::Real)
+    
+    R = [Cx Cy 0 0 0 0;
+        -Cy Cx 0 0 0 0;
+        0 0 1 0 0 0;
+        0 0 0 Cx Cy 0;
+        0 0 0 -Cy Cx 0;
+        0 0 0 0 0 1]
+    
+    return R
+end
+
+"""
+Rotation matrix for a 2D frame element using start and end points
+
+positionStart = [xstart, ystart]: position of starting node
+positionEnd = [xend, yend]: position of ending node
+L: length of element
+"""
+function R2DFrame(positionStart::Vector{Real}, positionEnd::Vector{Real}, L::Real)
+    
+    Cx, Cy = (positionEnd .- positionStart) ./ L
+
+    R = [Cx Cy 0 0 0 0;
+        -Cy Cx 0 0 0 0;
+        0 0 1 0 0 0;
+        0 0 0 Cx Cy 0;
+        0 0 0 -Cy Cx 0;
+        0 0 0 0 0 1]
+    
+    return R
+end
+
+"""
+Rotation matrix for a 3D frame element
+Cx, Cy, Cz are the directional cosines of the element in global coordinates
+Cx, Cy, Cz = (endPosition - startPosition) / length
+
+Ψ is the roll angle of the element with respect to local x axis
+By default, Ψ = π/2, which ensures that the local Z axis is parallel to the XY ground plane, IE the strong axis is fully utilized under global gravity loading
+"""
+function R3DFrame(Cx::Real, Cy::Real, Cz::Real; Ψ = pi/2)
+
+    xLocal = [Cx, Cy, Cz]
+
+    if norm(cross(xLocal, globalY)) < tol #special case for horizontal members aligned with global Y
+        Λ = [0. Cy 0.;
+            -Cy*cos(Ψ) 0 sin(Ψ);
+            Cy*sin(Ψ) 0 cos(Ψ)]
+    else # all other
+        b1 = (-Cx * Cy * cos(Ψ) - Cz * sin(Ψ)) / sqrt(Cx^2 + Cz^2)
+        b2 = sqrt(Cx^2 + Cz^2) * cos(Ψ)
+        b3 = (-Cy * Cz * cos(Ψ) + Cx * sin(Ψ)) / sqrt(Cx^2 + Cz^2)
+
+        c1 = (Cx * Cy * sin(Ψ) - Cz * cos(Ψ)) / sqrt(Cx^2 + Cz^2)
+        c2 = -sqrt(Cx^2 + Cz^2) * sin(Ψ)
+        c3 = (Cy * Cz * sin(Ψ) + Cx * cos(Ψ)) / sqrt(Cx^2 + Cz^2)
+
+        Λ = [Cx Cy Cz; 
+            b1 b2 b3; 
+            c1 c2 c3]
+    end
+    
+    R = [Λ zeros(3,9); zeros(3,3) Λ zeros(3,6); zeros(3,6) Λ zeros(3,3); zeros(3,9) Λ]
+
+    return R
+end
+
+"""
+Rotation matrix for a 3D frame element
+positionStart = [xstart, ystart, zstart]: position of starting node
+positionEnd = [xend, yend, zend]: position of ending node
+L: length of element
+
+Ψ is the roll angle of the element with respect to local x axis
+By default, Ψ = π/2, which ensures that the local Z axis is parallel to the XY ground plane, IE the strong axis is fully utilized under global gravity loading
+"""
+function R3DFrame(positionStart::Vector{Real}, positionEnd::Vector{Real}, L::Real; Ψ = pi/2)
+    
+    xLocal = normalize(positionEnd .- positionStart)
+    Cx, Cy, Cz = xlocal
+
+    if norm(cross(xLocal, globalY)) < tol #special case for horizontal members aligned with global Y
+        Λ = [0. Cy 0.;
+            -Cy*cos(Ψ) 0 sin(Ψ);
+            Cy*sin(Ψ) 0 cos(Ψ)]
+    else # all other
+        b1 = (-Cx * Cy * cos(Ψ) - Cz * sin(Ψ)) / sqrt(Cx^2 + Cz^2)
+        b2 = sqrt(Cx^2 + Cz^2) * cos(Ψ)
+        b3 = (-Cy * Cz * cos(Ψ) + Cx * sin(Ψ)) / sqrt(Cx^2 + Cz^2)
+
+        c1 = (Cx * Cy * sin(Ψ) - Cz * cos(Ψ)) / sqrt(Cx^2 + Cz^2)
+        c2 = -sqrt(Cx^2 + Cz^2) * sin(Ψ)
+        c3 = (Cy * Cz * sin(Ψ) + Cx * cos(Ψ)) / sqrt(Cx^2 + Cz^2)
+
+        Λ = [Cx Cy Cz; 
+            b1 b2 b3; 
+            c1 c2 c3]
+    end
+    
+    R = [Λ zeros(3,9); zeros(3,3) Λ zeros(3,6); zeros(3,6) Λ zeros(3,3); zeros(3,9) Λ]
+
+    return R
+end
---
+++ stifnessMatrices.jl
@@ -0,0 +1,190 @@
+
+"""
+Truss element
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+"""
+function KTruss(E::Real, A::Real, L::Real)
+    return E * A / L .* [1 -1; -1 1]
+end
+
+"""
+3D beam element
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+G: Shear Modulus
+Izz: Moment of Inertia in local Z (strong)
+Iyy: Moment of Inertia in local Y (weak)
+J: Torsional Constant
+"""
+function K3DFrame(E::Real, A::Real, L::Real, G::Real, Izz::Real, Iyy::Real, J::Real)
+    k = E / L^3 * [
+        A*L^2 0 0 0 0 0 -A*L^2 0 0 0 0 0;
+        0 12Izz 0 0 0 6L*Izz 0 -12Izz 0 0 0 6L*Izz;
+        0 0 12Iyy 0 -6L*Iyy 0 0 0 -12Iyy 0 -6L*Iyy 0;
+        0 0 0 G*J*L^2/E 0 0 0 0 0 -G*J*L^2/E 0 0;
+        0 0 -6L*Iyy 0 4L^2*Iyy 0 0 0 6L*Iyy 0 2L^2*Iyy 0;
+        0 6L*Izz 0 0 0 4L^2*Izz 0 -6L*Izz 0 0 0 2L^2*Izz;
+        -A*L^2 0 0 0 0 0 A*L^2 0 0 0 0 0;
+        0 -12Izz 0 0 0 -6L*Izz 0 12Izz 0 0 0 -6L*Izz;
+        0 0 -12Iyy 0 6L*Iyy 0 0 0 12Iyy 0 6L*Iyy 0;
+        0 0 0 -G*J*L^2/E 0 0 0 0 0 G*J*L^2/E 0 0;
+        0 0 -6L*Iyy 0 2L^2*Iyy 0 0 0 6L*Iyy 0 4L^2*Iyy 0;
+        0 6L*Izz 0 0 0 2L^2*Izz 0 -6L*Izz 0 0 0 4L^2*Izz
+        ]
+
+    return k
+end
+
+"""
+3D beam element with a hinge at the starting node
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+Izz: Moment of Inertia in local Z (strong)
+Iyy: Moment of Inertia in local Y (weak)
+"""
+function K3DFrame_FreeFixed(E::Real, A::Real, L::Real, Izz::Real, Iyy::Real)
+
+    k = E / L^3 .* [A*L^2 0 0 0 0 0 -A*L^2 0 0 0 0 0;
+        0 3Izz 0 0 0 0 0 -3Izz 0 0 0 3L*Izz;
+        0 0 3Iyy 0 0 0 0 0 -3Iyy 0 -3L*Iyy 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        -A*L^2 0 0 0 0 0 A*L^2 0 0 0 0 0;
+        0 -3Izz 0 0 0 0 0 3Izz 0 0 0 -3L*Izz;
+        0 0 -3Iyy 0 0 0 0 0 3Iyy 0 3L*Iyy 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 -3L*Iyy 0 0 0 0 0 3L*Iyy 0 3L^2*Iyy 0;
+        0 3L*Izz 0 0 0 0 0 -3L*Izz 0 0 0 3L^2*Izz    
+    ]
+
+    return k
+end
+
+"""
+3D beam element with a hinge at the ending node
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+Izz: Moment of Inertia in local Z (strong)
+Iyy: Moment of Inertia in local Y (weak)
+"""
+function K3DFrame_FixedFree(E::Real, A::Real, L::Real, Izz::Real, Iyy::Real)
+
+    k = E / L^3 .* [A*L^2 0 0 0 0 0 -A*L^2 0 0 0 0 0;
+        0 3Izz 0 0 0 3L*Izz 0 -3Izz 0 0 0 0;
+        0 0 3Iyy 0 -3L*Iyy 0 0 0 -3Iyy 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 -3L*Iyy 0 3L^2*Iyy 0 0 0 3L*Iyy 0 0 0;
+        0 3L*Izz 0 0 0 3L^2*Izz 0 -3L*Izz 0 0 0 0 ;
+        -A*L^2 0 0 0 0 0 A*L^2 0 0 0 0 0;
+        0 -3Izz 0 0 0 -3L*Izz 0 3Izz 0 0 0 0;
+        0 0 -3Iyy 0 3L*Iyy 0 0 0 3Iyy 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0    
+    ]
+
+    return k
+end
+
+"""
+3D beam element with a hinges at both ends (truss element) -- use sparingly
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+"""
+function K3DFrame_FreeFree(E::Real, A::Real, L::Real)
+
+    k = zeros(12, 12)
+    k[1, 1] = k[7, 7] = 1
+    k[1, 7] = k[7, 1] = -1
+
+    return E * A / L .* k
+end
+
+"""
+2D beam element
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+I: Moment of Inertia in out-of-plane axis
+"""
+function K2DFrame2(E::Real, A::Real, L::Real, I::Real)
+
+    k = E * I / L^3 .* [A*L^2/I 0 0 -A*L^2/I 0 0;
+        0 12 6L 0 -12 6L;
+        0 6L 4L^2 0 -6L 2L^2;
+        -A*L^2/I 0 0 A*L^2/I 0 0;
+        0 -12 -6L 0 12 -6L;
+        0 6L 2L^2 0 -6L 4L^2]
+
+    return k
+end
+
+"""
+2D beam element with hinge at starting node
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+I: Moment of Inertia in out-of-plane axis
+"""
+function K2DFrame_FreeFixed(E::Real, A::Real, L::Real, I::Real)
+
+    k = E * I / L^3 .* [A*L^2/I 0 0 -A*L^2/I 0 0;
+        0 3 0 0 -3 3L;
+        0 0 0 0 0 0;
+        -A*L^2/I 0 0 A*L^2/I 0 0;
+        0 -3 0 0 3 -3L;
+        0 3L 0 0 -3L 3L^2]
+
+    return k
+end
+
+"""
+2D beam element with hinge at ending node
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+I: Moment of Inertia in out-of-plane axis
+"""
+function K2DFrame_FixedFree(E::Real, A::Real, L::Real, I::Real)
+
+    k = E * I / L^3 .* [A*L^2/I 0 0 -A*L^2/I 0 0;
+        0 3 3L 0 -3 0;
+        0 3L 3L^2 0 -3L 0;
+        -A*L^2/I 0 0 A*L^2/I 0 0;
+        0 -3 -3L 0 3 0;
+        0 0 0 0 0 0]
+
+    return k
+end
+
+"""
+2D beam element with hinges at both ends (ie a truss)
+
+E: Modulus of Elasticity
+A: Cross section area
+L: Element length
+I: Moment of Inertia in out-of-plane axis
+"""
+function K2DFrame_FixedFree(E::Real, A::Real, L::Real)
+
+    k = zeros(6, 6)
+    k[1, 1] = k[4, 4] = 1
+    k[1, 4] = k[4, 1] = -1
+
+    return k
+end
